870674365777b97685ba6bf839b5d7726aa0775c
jcoon3 <jcoon3@fingerlakes.edu>
2025-09-30 19:09:14 +0000

    compliment_02.py take 1

--- compliment_02.py
+++ compliment_02.py
@@ -1,80 +1,21 @@
-# FILE NAME - compliment_02.py
+# Name: Justin Coon
+# Date: 09-29-2025
+# Description A program that asks a user if the want a compliment.
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# Variable declaration asking the user if they would like a compliment
+compliment = input("Would you like a compliment: ")
 
-
-
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
-
-
-
-########## ENTER YER CODE BELOW THIS LINE ##########
-
-
-
-
-
-
-
-
-
-
-########### END YER CODE ABOVE THIS LINE ###########
-
+# If statement to see if the user inputs the correct condition
+if compliment == 'yes':
     
+    # Prints the compliment if the condition is met
+    print("You have wonderful eyes.")
 
+# Else clause that prints a staement if the condition is not met    
+else:
+    
+    print("No compliment for you!")
 
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-
-'''
-Would you like a compliment? yes
-You have wonderful eyes.
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? Yes
-No compliment for you!
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? y
-No compliment for you!
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? no
-No compliment for you!
-Thank you for playing.
-'''
-
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
-'''
-
-1. Did you struggle with this lab (YES/NO)?
-
-
-
-
-
-
-
-'''
+# Prints a thank you message    
+print("Thank you for playing.")
+    
